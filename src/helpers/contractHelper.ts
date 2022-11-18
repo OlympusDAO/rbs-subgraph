d3523657808f56cabbc2f1c66f6be048093ffb53
Jem <0x0xjem@gmail.com>
2022-11-18 20:44:45 +0400

    mainnet deployment

--- src/helpers/contractHelper.ts
+++ src/helpers/contractHelper.ts
@@ -1,12 +1,13 @@
 import { Address } from "@graphprotocol/graph-ts";
 
+import { PRICE_CONTRACT, RANGE_CONTRACT } from "../constants";
+
 export const CHAIN_GOERLI = "Goerli";
 export const CHAIN_MAINNET = "Mainnet";
 
 const ADDRESS_CHAIN_MAPPING = new Map<string, string>();
-ADDRESS_CHAIN_MAPPING.set("0x9ecda630626a3aa9ef24a53c4faca1ce76a1a508".toLowerCase(), CHAIN_GOERLI);
-ADDRESS_CHAIN_MAPPING.set("0x6d39cdfa180974c5e1ac6fd325a1718f2fd4412f".toLowerCase(), CHAIN_GOERLI);
-// TODO Add mainnet addresses
+ADDRESS_CHAIN_MAPPING.set(PRICE_CONTRACT.toLowerCase(), CHAIN_MAINNET);
+ADDRESS_CHAIN_MAPPING.set(RANGE_CONTRACT.toLowerCase(), CHAIN_MAINNET);
 
 /**
  * Returns the chain for the given contract address.
