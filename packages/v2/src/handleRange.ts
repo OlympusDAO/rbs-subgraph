64687c0f0ce4004308caf707ce17228cbd4a5f51
Jem <0x0xjem@gmail.com>
2024-06-24 21:56:01 +0400

    chore: linting

--- packages/v2/src/handleRange.ts
+++ packages/v2/src/handleRange.ts
@@ -1,6 +1,7 @@
 import { Address, ethereum } from "@graphprotocol/graph-ts";
-import { createRangeV2Snapshot } from "./rangeV2Snapshot";
+
 import { ERC20_OHM_V2 } from "./constants";
+import { createRangeV2Snapshot } from "./rangeV2Snapshot";
 
 export function handleBlock(block: ethereum.Block): void {
     createRangeV2Snapshot(Address.fromString(ERC20_OHM_V2), block);
